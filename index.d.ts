d4721511cdc2d8156aade25146a20def495df3f7
Ran Ju <ryan@trinnylondon.com>
2020-08-13 23:51:53 +0100

    Fixed index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,32 +2,45 @@
 
 import { EventEmitter } from 'events';
 import { Stream } from 'stream';
-import * as grpc from '@grpc/grpc-js';
+import {
+  ServerUnaryCall,
+  ServerReadableStream,
+  ServerWritableStream,
+  ServerDuplexStream,
+  Server,
+  Metadata
+} from '@grpc/grpc-js';
 
 type GrpcRequest = any;
 
 type GrpcResponse = any;
 
 type GrpcCall =
-  grpc.ServerUnaryCall<any> |
-  grpc.ServerReadableStream<any> |
-  grpc.ServerWriteableStream<any> |
-  grpc.ServerDuplexStream<any, any>
+  ServerUnaryCall<any, any> |
+  ServerReadableStream<any, any> |
+  ServerWritableStream<any, any> |
+  ServerDuplexStream<any, any>
 
 declare class Mali extends EventEmitter {
   constructor(path?: any, name?: string | ReadonlyArray<string>, options?: any);
+
   name: string;
   env: string;
   ports: ReadonlyArray<number>;
   silent: boolean;
   context: object
 
-  addService (path: any, name: string | ReadonlyArray<string>, options?: any): void;
-  use (service?: any, name?: any, fns?: any): void;
-  start (port: number | string, creds?: any, options?: any): Promise<grpc.Server>;
-  toJSON (): any;
-  close (): Promise<void>;
-  inspect (): any;
+  addService(path: any, name: string | ReadonlyArray<string>, options?: any): void;
+
+  use(service?: any, name?: any, fns?: any): void;
+
+  start(port: number | string, creds?: any, options?: any): Promise<Server>;
+
+  toJSON(): any;
+
+  close(): Promise<void>;
+
+  inspect(): any;
 }
 
 declare namespace Mali {
@@ -45,37 +58,51 @@ declare namespace Mali {
     type: string;
     metadata: any;
     locals: object;
-    get (field: string): any;
-    set (field: any, val?: any): void;
-    sendMetadata (md: any): void;
-    getStatus (field: string): any;
-    setStatus (field: any, val?: any): void;
+
+    get(field: string): any;
+
+    set(field: any, val?: any): void;
+
+    sendMetadata(md: any): void;
+
+    getStatus(field: string): any;
+
+    setStatus(field: any, val?: any): void;
   }
 
   class Request {
     constructor(call: any, type: string);
+
     call: any;
     type: string;
     metadata: any;
     req: GrpcRequest;
 
-    getMetadata (): grpc.Metadata;
-    get (field: string): any;
+    getMetadata(): Metadata;
+
+    get(field: string): any;
   }
 
   class Response {
     constructor(call: any, type: string);
+
     call: any;
     type: string;
     metadata: any;
     status: any;
     res: GrpcResponse;
-    set (field: any, val?: any): void;
-    get (field: string): any;
-    getMetadata (): grpc.Metadata;
-    sendMetadata (md?: any): void;
-    getStatus (field: string): any;
-    setStatus (field: any, val?: any): void;
+
+    set(field: any, val?: any): void;
+
+    get(field: string): any;
+
+    getMetadata(): Metadata;
+
+    sendMetadata(md?: any): void;
+
+    getStatus(field: string): any;
+
+    setStatus(field: any, val?: any): void;
   }
 
 }
